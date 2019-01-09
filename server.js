02bf06e2b2e8d6c7715f30b30764ca7ea85165df
James Studd <james.studd@live.co.uk>
2019-01-09 20:00:12 +0000

    Added bower, bootstrap to forms, navigation bar for the demo

--- server.js
+++ server.js
@@ -21,6 +21,8 @@ function InitAppUses() {
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
     
+    app.use(express.static('public'));
+
     app.set('view engine', 'pug')
 }
 
