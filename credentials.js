8f7cb26322b979868f4fec62465ea0ba19150ff9
James Studd <james.studd@live.co.uk>
2019-02-23 23:21:57 +0000

    Example of credentials

---
+++ credentials.js
@@ -0,0 +1,22 @@
+var config = {
+    'development': {
+        database: 'database link',
+        user: 'username',
+        password: 'password',
+        secret: 'secret'
+    },
+    'test': {
+        database: 'database link',
+        user: 'username',
+        password: 'password',
+        secret: 'secret'
+    },
+    'production': {
+        database: 'database link',
+        user: 'username',
+        password: 'password',
+        secret: 'secret'
+    }
+}
+
+module.exports = config;
